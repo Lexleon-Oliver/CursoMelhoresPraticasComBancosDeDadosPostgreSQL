bdc008b7b79660ee7dc12e7769e2ca83775b3364
Lexleon-Oliver <lexleonmg@gmail.com>
2021-11-24 14:00:51 -0300

    Iniciar Curso

---
+++ postgres/src/main/java/com/development/database/postgres/PostgresApplication.java
@@ -0,0 +1,13 @@
+package com.development.database.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresApplication.class, args);
+	}
+
+}
